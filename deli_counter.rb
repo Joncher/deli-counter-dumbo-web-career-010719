bf280d22743958b872e98db2b86d2e0e69b59705
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 15:22:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(katz_deli)
   if katz_deli.size > 0
     serving = ""
     serving = katz_deli.shift
-    return "Now serving: #{serving}"
-  else return "There is nobody waiting to be served!" 
+    puts "Now serving: #{serving}"
+  else puts "There is nobody waiting to be served!" 
   end
 end
