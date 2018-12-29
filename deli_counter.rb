56bcb4162abf80666faf04941cdc6d0eb3f51ed5
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 14:49:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,8 @@ def line(katz_deli)
   if katz_deli.size > 0
     katz_deli.collect do | name |
       "#{1+katz_deli.index(name)}. #{name}"
-      binding.pry
     end
-    
+    puts katz_deli
   end
 end
 
