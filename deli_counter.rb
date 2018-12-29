ce556f1d64e357b4d5493c4230fe3dde2f64f4a9
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 14:32:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."   
+  end
+  if katz_deli.size > 0
+    katz_deli.collect do | name |
+      #{1+katz_deli.index(name)}. #{name}
+    end
+  end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli.push(name)
+end
